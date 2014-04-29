a2fed9b225f2c17a9f1ab28a2740123dd864909d
Anton Podviaznikov <podviaznikov@gmail.com>
2014-04-30 00:50:10 +0200

    bump versions

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :url "https://github.com/hashobject/mandrill"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.8"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "0.9.1"]
                  [cheshire "5.3.1"]])
